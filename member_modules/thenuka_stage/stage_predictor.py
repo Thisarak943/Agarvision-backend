8459c4eb491ced30124fa42aa1f33d2f322b056c
thenukarathnamalala <rathnamalalathenuka@gmail.com>
2026-03-06 16:34:54 +0530

    update the stage_predicor file

--- member_modules/thenuka_stage/stage_predictor.py
+++ member_modules/thenuka_stage/stage_predictor.py
@@ -35,11 +35,11 @@ def load_all():
 # =========================
 # Image Preprocessing
 # =========================
-def preprocess_image(image: Image.Image, target_size=(160, 160)):
+def preprocess_image(image: Image.Image, target_size=(224, 224)):
     image = image.convert("RGB")
     image = image.resize(target_size)
     img_array = np.array(image).astype("float32") / 255.0
-    img_array = np.expand_dims(img_array, axis=0)  # (1, H, W, C)
+    img_array = np.expand_dims(img_array, axis=0)  
     return img_array
 
 
